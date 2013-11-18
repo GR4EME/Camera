cca46e0300b094743f0bdc1b1a5aa43b7f117c78
Graeme Littlewood <graemelittlewood@graemes-mbp.lan>
2013-11-18 19:02:40 +0000

    Initial Commit

---
+++ Camera/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Camera
+//
+//  Created by Graeme Littlewood on 18/11/2013.
+//  Copyright (c) 2013 Graeme Littlewood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
